f82fb7c7a1c4faf812b69d76cb83ca6927064efa
Cleytoni De Paula <cleytonidepaula8@gmail.com>
2023-03-21 15:09:03 -0300

    Removido Db.json

---
+++ agenda/local_settings.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'sitedjango',
+        "HOST": '127.0.0.1',
+        "PORT": '3306',
+        "USER": 'root',
+        "PASSWORD": '',
+        }
+    }
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
--- agenda/settings.py
+++ agenda/settings.py
@@ -148,3 +148,8 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
